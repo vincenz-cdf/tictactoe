f6fc9aa5ebcec24d3b1e84bd9e83d6fc571553c5
Vincent <85881442+vincenz-cdf@users.noreply.github.com>
2022-08-15 10:21:52 +0200

    debut reset

--- tictactoe/ViewController.swift
+++ tictactoe/ViewController.swift
@@ -30,13 +30,66 @@ class ViewController: UIViewController {
     
     var NOUGHT = "O"
     var CROSS = "X"
+    var board = [UIButton]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initBoard()
+    }
+    
+    func initBoard() {
+        board.append(a1)
+        board.append(a2)
+        board.append(a3)
+        board.append(b1)
+        board.append(b2)
+        board.append(b3)
+        board.append(c1)
+        board.append(c2)
+        board.append(c3)
     }
 
     @IBAction func boardTapAction(_ sender: UIButton) {
         addToBoard(sender)
+        
+        if(fullBoard())
+        {
+            resultAlert(title: "Draw")
+        }
+    }
+    
+    func resultAlert(title: String) {
+        let ac = UIAlertController(title: title, message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Reset", style: .default, handler: { (_) in self.resetBoard()
+        }))
+        self.present(ac, animated: true)
+    }
+    
+    func resetBoard() {
+        for button in board {
+            button.setTitle(nil, for: .normal)
+            button.isEnabled = true
+        }
+        if firstTurn == Turn.Nought
+        {
+            firstTurn = Turn.Cross
+            turnLabel.text = CROSS
+        }
+        else if firstTurn == Turn.Cross
+        {
+            firstTurn = Turn.Nought
+            turnLabel.text = NOUGHT
+        }
+        currentTurn = firstTurn
+    }
+    
+    func fullBoard() -> Bool {
+        for button in board {
+            if button.title(for: .normal) == nil {
+                return false
+            }
+        }
+        return true
     }
     
     func addToBoard(_ sender: UIButton) {
@@ -51,7 +104,18 @@ class ViewController: UIViewController {
                 currentTurn = Turn.Nought
                 turnLabel.text = NOUGHT
             }
+            sender.isEnabled = false
         }
     }
+    
+    func checkForVictory(_ s: String) -> Bool {
+        return false;
+    }
+    
+    /*
+    func thisSymbol() {
+        
+    }
+     */
 }
 
